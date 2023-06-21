0ec204744cef419835b5de4aaa5909285c1667c7
Azza Abdalla <96057396+AzzaAbdalla@users.noreply.github.com>
2023-06-21 16:51:37 +0300

    Add files via upload

---
+++ src/Modal.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useContext } from "react";
+import { FaTimes } from "react-icons/fa";
+import { AppContext } from "./context";
+const Modal = () => {
+  const { isModalOpen, closeModal } = useContext(AppContext);
+  return (
+    <div
+      className={`${
+        isModalOpen ? "modal-overlay show-modal" : "modal-overlay"
+      }`}
+    >
+      <div className="modal-container">
+        <h3>model content</h3>
+        <button onClick={closeModal} className="close-modal-btn">
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
---
+++ src/context.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isModalOpen,
+        isSidebarOpen,
+        openModal,
+        closeModal,
+        openSidebar,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export { AppContext, AppProvider };
